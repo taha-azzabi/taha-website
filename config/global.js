84c6b88f0277805e8b28cd51f34f95d2b1baa994
taha <taha.azzabi@gmail.com>
2020-01-09 12:21:42 +0100

    fix font size

--- config/global.js
+++ config/global.js
@@ -1,3 +1,2 @@
-export const CV =
-  'https://firebasestorage.googleapis.com/v0/b/taha-azzabi.appspot.com/o/0.0.4-CV%20_taha_azzabi_web_dev.pdf?alt=media&token=9257fa96-7e20-42d7-b694-51c2b49c15a3'
-export const SOURCE_CODE = 'https://github.com/taha-azzabi/taha-work'
+export const CV = ''
+export const SOURCE_CODE = 'https://github.com/taha-azzabi/taha-website.git'
